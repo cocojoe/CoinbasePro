26c130b93b3843b808e4c62f58803df37b0e232a
Martin Walsh <martin.walsh@gmail.com>
2018-06-06 09:40:02 +0100

    Initial Commit

---
+++ CoinbasePro/CoinbasePro.h
@@ -0,0 +1,19 @@
+//
+//  CoinbasePro.h
+//  CoinbasePro
+//
+//  Created by Martin on 06/06/2018.
+//  Copyright © 2018 Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoinbasePro.
+FOUNDATION_EXPORT double CoinbaseProVersionNumber;
+
+//! Project version string for CoinbasePro.
+FOUNDATION_EXPORT const unsigned char CoinbaseProVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoinbasePro/PublicHeader.h>
+
+
