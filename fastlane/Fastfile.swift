d460a7240fd6a7e3937997708c0c1c1392c1a9cc
Martin Walsh <martin.walsh@gmail.com>
2018-06-12 22:29:48 +0100

    Added Fastlane Added Code Coverage Config Added Circl CI 2.0 (Needs a Subscription) Added Travis YML

---
+++ fastlane/Fastfile.swift
@@ -0,0 +1,21 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+	func customLane() {
+        	desc("Description of what the lane does")
+		// add actions here: https://docs.fastlane.tools/actions
+	}
+
+	func testLane() {
+		desc("Description of what the lane does")
+        	runTests(project: "CoinbasePro.xcodeproj", devices: ["iPhone 8"], scheme: "CoinbasePro.iOS")
+	}
+}
