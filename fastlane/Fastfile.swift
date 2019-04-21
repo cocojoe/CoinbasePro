a1718bd94b798949fb069d181f5d7c91914ffb35
Martin Walsh <martin.walsh@gmail.com>
2019-04-21 09:11:20 +0100

    Update Fastlane Add Circle CI 2.0 Support Drop Travis

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -9,13 +9,14 @@
 import Foundation
 
 class Fastfile: LaneFile {
-	func customLane() {
-        	desc("Description of what the lane does")
-		// add actions here: https://docs.fastlane.tools/actions
-	}
-
+    
+    func bootstrapLane() {
+        desc("Bootstrap")
+        carthage(command: "bootstrap", platform: "iOS", cacheBuilds: true)
+    }
+    
 	func testLane() {
-		desc("Description of what the lane does")
-        	runTests(project: "CoinbasePro.xcodeproj", device: "iPhone 8", scheme: "CoinbasePro.iOS", clean: true, codeCoverage: true)
+		desc("Run tests")
+        runTests(project: "CoinbasePro.xcodeproj", scheme: "CoinbasePro.iOS", device: "iPhone 8", clean: true, codeCoverage: true)
 	}
 }
